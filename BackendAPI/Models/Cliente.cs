ccf4114a7566fbdb30585524ea9de61ae16b01c2
JavierMung <fjmg@123lease.com.mx>
2024-01-17 00:12:45 -0600

    arreglo repositorio para juntar el front y backend

--- BackendAPI/Models/Cliente.cs
+++ BackendAPI/Models/Cliente.cs
@@ -9,9 +9,9 @@ public partial class Cliente
 
     public string Nombre { get; set; } = null!;
 
-    public string? Dirección { get; set; }
+    public string  ? Dirección { get; set; }
 
-    public string? NumeroTelefono { get; set; }
+    public string   ? NumeroTelefono { get; set; }
 
     public virtual ICollection<Pedido> Pedidos { get; set; } = new List<Pedido>();
 }
